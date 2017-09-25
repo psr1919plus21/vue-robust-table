8985f31a397532a62ca05289ea6199714c239c41
Filipp Gaponenko <psr1919plus21@gmail.com>
2017-09-26 01:00:37 +0300

    :rocket: Initial table layout.

--- client/src/app/mainPage.js
+++ client/src/app/mainPage.js
@@ -1,10 +1,198 @@
 /* global Vue */
-import './components/MyComponent';
+import './components/vue-robust-table';
 
 new Vue({
   el: '#app',
+  components: ['VueRobustTable'],
   data: {
-    message: 'Hello Vue!'
+    dataTableRows: [
+      {
+        state: 'active',
+        content: [
+          {
+            title: 'id',
+            value: 1
+          },
+          {
+            title: 'color',
+            value: '#334345'
+          },
+          {
+            title: 'size',
+            value: 'large'
+          },
+          {
+            title: 'geo',
+            value: 'Sevastopol'
+          },
+          {
+            title: 'animal',
+            value: 'cat'
+          },
+
+        ]
+      },
+      {
+        state: 'default',
+        content: [
+          {
+            title: 'id',
+            value: 2
+          },
+          {
+            title: 'color',
+            value: '#45a32f'
+          },
+          {
+            title: 'size',
+            value: 'medium'
+          },
+          {
+            title: 'geo',
+            value: 'Moscow'
+          },
+          {
+            title: 'animal',
+            value: 'dog'
+          },
+        ]
+      },
+      {
+        state: 'default',
+        content: [
+          {
+            title: 'id',
+            value: 2
+          },
+          {
+            title: 'color',
+            value: '#45a32f'
+          },
+          {
+            title: 'size',
+            value: 'medium'
+          },
+          {
+            title: 'geo',
+            value: 'Moscow'
+          },
+          {
+            title: 'animal',
+            value: 'dog'
+          },
+        ]
+      },
+      {
+        state: 'default',
+        content: [
+          {
+            title: 'id',
+            value: 2
+          },
+          {
+            title: 'color',
+            value: '#45a32f'
+          },
+          {
+            title: 'size',
+            value: 'medium'
+          },
+          {
+            title: 'geo',
+            value: 'Moscow'
+          },
+          {
+            title: 'animal',
+            value: 'dog'
+          },
+        ]
+      },
+      {
+        state: 'default',
+        content: [
+          {
+            title: 'id',
+            value: 2
+          },
+          {
+            title: 'color',
+            value: '#45a32f'
+          },
+          {
+            title: 'size',
+            value: 'medium'
+          },
+          {
+            title: 'geo',
+            value: 'Moscow'
+          },
+          {
+            title: 'animal',
+            value: 'dog'
+          },
+        ]
+      },
+      {
+        state: 'default',
+        content: [
+          {
+            title: 'id',
+            value: 2
+          },
+          {
+            title: 'color',
+            value: '#45a32f'
+          },
+          {
+            title: 'size',
+            value: 'medium'
+          },
+          {
+            title: 'geo',
+            value: 'Moscow'
+          },
+          {
+            title: 'animal',
+            value: 'dog'
+          },
+        ]
+      },
+      {
+        state: 'default',
+        content: [
+          {
+            title: 'id',
+            value: 2
+          },
+          {
+            title: 'color',
+            value: '#45a32f'
+          },
+          {
+            title: 'size',
+            value: 'medium'
+          },
+          {
+            title: 'geo',
+            value: 'Moscow'
+          },
+          {
+            title: 'animal',
+            value: 'dog'
+          },
+        ]
+      }
+    ],
+
+    dataTableSettings: {
+      tableHeader: true
+    }
+  },
+  methods: {
+    toggleHeader() {
+      console.log('toggleHeader');
+      this.dataTableSettings.tableHeader = !this.dataTableSettings.tableHeader;
+    }
   },
   mounted: function() {
     this.$el.classList.remove('vue-app-preload');
