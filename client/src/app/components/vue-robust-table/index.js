c84a650b4896c1d6a43809110545e648a2387d07
Filipp Gaponenko <psr1919plus21@gmail.com>
2017-09-26 11:31:23 +0300

    :rocket: Enable edit mode by doubleclick.

--- client/src/app/components/vue-robust-table/index.js
+++ client/src/app/components/vue-robust-table/index.js
@@ -43,5 +43,48 @@ export default Vue.component('vue-robust-table', {
         'vue-robust-table__cell_no-borders': !this.showBorders,
       }
     }
+  },
+  methods: {
+    editModeEnable(cell, row_index, cell_index) {
+      console.log('enable edit mode');
+      let currentCell = this.rows[row_index].content[cell_index];
+      let currentInput = this.$refs[`cell-input-${row_index}-${cell_index}`];
+
+      Vue.set(currentCell, 'editProcess', true);
+      Vue.nextTick(() => {
+        currentInput[0].dispatchEvent(new Event('focus'));
+        console.log(currentInput);
+      });
+    }
   }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ddf
--- client/src/app/components/vue-robust-table/vue-robust-table-template.js
+++ client/src/app/components/vue-robust-table/vue-robust-table-template.js
@@ -9,12 +9,16 @@ export const template = `
       </div>
 
       <div class="vue-robust-table__row"
-         v-for="row in rows">
+         v-for="(row, row_index) in rows">
           <div class="vue-robust-table__cell"
-            :class="cellModifyers"
-            v-for="cell in row.content">
-            <div class="vue-robust-table__value"
+            :class="[cellModifyers, {'vue-robust-table__cell_editprocess': cell.editProcess}]"
+            v-for="(cell, cell_index) in row.content"
+            @dblclick.left="editModeEnable(cell, row_index, cell_index)">
+            <div v-show="!cell.editProcess" class="vue-robust-table__value"
               v-text="cell.value"></div>
+            <div v-show="cell.editProcess" class="vue-robust-table__input-wrapper">
+              <input :ref="'cell-input-' + row_index + '-' + cell_index" type="text" class="vue-robust-table__input">
+            </div>
 
           </div>
       </div>
